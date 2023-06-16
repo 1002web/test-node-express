c0df0ef29fc9837f6d49edd0be956d5f527b23bc
Joffrey Schoemacker <1002websolutions@gmail.com>
2023-06-16 15:29:39 +0200

    added port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.get("/", function(request, response)  {
    
